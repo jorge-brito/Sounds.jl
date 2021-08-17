4d2b084102b1779991cc03f25914eafb3a2649fe
Jorge Brito <jorge.brito.json@gmail.com>
2021-08-17 13:36:10 -0300

    More tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,10 @@
 using Sounds
 
 sound = Sound(joinpath(@__DIR__, "piano.wav"))
+music = Music(joinpath(@__DIR__, "piano.wav"))
 
 @show sound
+Sounds.showprops(sound)
+
+@show music
+Sounds.showprops(music)
