0b846aae38a346630759e90d808e5305a452620b
Jorge Brito <jorge.brito.json@gmail.com>
2021-08-18 18:18:49 -0300

    Export more functions

--- src/Sounds.jl
+++ src/Sounds.jl
@@ -9,13 +9,30 @@ export Music
 export Sound
 export SoundBuffer
 
-export stop!
+export duration
+export getsamples
+export getstatus
+export islooping
+export ispaused
+export isplaying
+export isstopped
+export loop!
+export nchannels
+export nsamples
 export pause!
-export play!
-
-export volume!
+export pitch
 export pitch!
-export offset!
+export play!
+export samplepos
+export samplerate
+export stop!
+export timepos
+export timepos!
+export volume
+export volume!
+
+export getprops
+export showprops
 
 abstract type SoundObject{T} end
 abstract type AbstractSound{T} <: SoundObject{T} end
@@ -25,6 +42,7 @@ function Base.unsafe_convert(::Type{Ptr{T}}, x::SoundObject{T}) where {T}
 end
 
 include("utils.jl")
+include("methods.jl")
 include("soundbuffer.jl")
 include("sound.jl")
 include("music.jl")
